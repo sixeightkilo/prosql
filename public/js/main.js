f550be52a49f45ea81292340befbe7737a49c80f
Pankaj Kargirwar <kargirwar@protonmail.com>
2021-04-01 13:27:01 +0530

    Tested localhost communication

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,9 @@ class App {
         document.addEventListener('DOMContentLoaded', async () => {
             this.$para = document.getElementById('main')
             this.$para.innerHTML = 'Hello from JS!'
+            const response = await fetch('http://localhost:23890')
+            const json = await response.json()
+            console.log(JSON.stringify(json))
         })
     }
 }
