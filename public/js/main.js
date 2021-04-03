293bc5e134faa5be4199c705cf4bf19bce3bb3c3
Pankaj Kargirwar <kargirwar@protonmail.com>
2021-04-03 10:32:47 +0530

    Testing sessions

--- public/js/main.js
+++ public/js/main.js
@@ -3,26 +3,49 @@ class App {
         document.addEventListener('DOMContentLoaded', async () => {
             this.$para = document.getElementById('main')
             this.$para.innerHTML = 'Hello from JS!'
-            const response = await fetch('http://localhost:23890/ping')
-            const json = await response.json()
+
+            let params = {
+                user: 'server',
+                pass: 'dev-server',
+                ip: '127.0.0.1',
+                port: 3306,
+                db: 'test-generico'
+            }
+
+            let response = await fetch('http://localhost:23890/ping?' + new URLSearchParams(params))
+            let json = await response.json()
+            console.log(JSON.stringify(json))
+            
+            response = await fetch('http://localhost:23890/login?' + new URLSearchParams(params))
+            json = await response.json()
             console.log(JSON.stringify(json))
 
-            let ws = new WebSocket("ws://localhost:23890/echo");
-            ws.onopen = function(evt) {
-                console.log("OPEN");
-                ws.send("Hi")
-            }
-            ws.onclose = function(evt) {
-                console.log("CLOSE");
-                ws = null;
-            }
-            ws.onmessage = function(evt) {
-                console.log("RESPONSE: " + evt.data);
-            }
-            ws.onerror = function(evt) {
-                console.log("ERROR: " + evt.data);
+            let sessionId = json.data['session-id']
+            console.log(`sid: ${sessionId}`)
+
+            params = {
+                'session-id': sessionId
             }
 
+            response = await fetch('http://localhost:23890/check?' + new URLSearchParams(params))
+            json = await response.json()
+            console.log(JSON.stringify(json))
+
+            //let ws = new WebSocket("ws://localhost:23890/echo");
+            //ws.onopen = function(evt) {
+                //console.log("OPEN");
+                //ws.send("Hi")
+            //}
+            //ws.onclose = function(evt) {
+                //console.log("CLOSE");
+                //ws = null;
+            //}
+            //ws.onmessage = function(evt) {
+                //console.log("RESPONSE: " + evt.data);
+            //}
+            //ws.onerror = function(evt) {
+                //console.log("ERROR: " + evt.data);
+            //}
         })
     }
 }
