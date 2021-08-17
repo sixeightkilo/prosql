336e7683e7333273338cf3827fbdd96e7ebae5f0
Pankaj Kargirwar <kargirwar@protonmail.com>
2021-08-17 14:37:35 +0530

    Fixing AG errors

--- public/js/modules/cell-header.js
+++ public/js/modules/cell-header.js
@@ -8,45 +8,64 @@ const TAG = "cell-header"
 class CellHeader {
     init(params) {
         this.params = params;
+        //todo: Using generateNode causes lot of error in console
+        this.$header = document.createElement('div');
+        this.$header.innerHTML = `
+           <div class="customHeaderLabel">${this.params.displayName}</div>
+           <div class="customSortDownLabel inactive">
+               <i class="fa fa-long-arrow-alt-down"></i>
+           </div>
+           <div class="customSortUpLabel inactive">
+               <i class="fa fa-long-arrow-alt-up"></i>
+           </div>
+           <div class="customSortRemoveLabel inactive">
+               <i class="fa fa-times"></i>
+           </div>
+       `;
 
-        let templ = document.getElementById('table-header-template').innerHTML;
-        this.$header = Utils.generateNode(templ, {
-            'col': this.params.displayName
-        });
-
-        this.$sortDown = this.$header.querySelector('.customSortDownLabel');
-        this.$sortUp = this.$header.querySelector('.customSortUpLabel');
-        this.$sortRemove = this.$header.querySelector('.customSortRemoveLabel');
+        this.$sortDown = this.$header.querySelector(".customSortDownLabel");
+        this.$sortUp = this.$header.querySelector(".customSortUpLabel");
+        this.$sortRemove = this.$header.querySelector(".customSortRemoveLabel");
 
         if (this.params.enableSorting) {
             this.onSortAscRequestedListener = this.onSortRequested.bind(this, 'asc');
-            this.$sortDown.addEventListener(
-                'click',
-                this.onSortAscRequestedListener
-            );
-
+            this.$sortDown.addEventListener('click', this.onSortAscRequestedListener);
             this.onSortDescRequestedListener = this.onSortRequested.bind(this, 'desc');
-            this.$sortUp.addEventListener(
-                'click',
-                this.onSortDescRequestedListener
-            );
-
+            this.$sortUp.addEventListener('click', this.onSortDescRequestedListener);
             this.onRemoveSortListener = this.onSortRequested.bind(this, '');
-            this.$sortRemove.addEventListener(
-                'click',
-                this.onRemoveSortListener
-            );
+            this.$sortRemove.addEventListener('click', this.onRemoveSortListener);
 
-            this.params.column.addEventListener(
-                'sortChanged',
-                this.onSortChangedListener
-            );
+            this.onSortChangedListener = this.onSortChanged.bind(this);
+            this.params.column.addEventListener('sortChanged', this.onSortChangedListener);
+            this.onSortChanged();
         } else {
             this.$header.removeChild(this.$sortDown);
             this.$header.removeChild(this.$sortUp);
             this.$header.removeChild(this.$sortRemove);
         }
+    }
 
+    onSortChanged() {
+        const deactivate = toDeactivateItems => {
+            toDeactivateItems.forEach(toDeactivate => {
+                toDeactivate.className = toDeactivate.className.split(' ')[0]
+            });
+        }
+
+        const activate = toActivate => {
+            toActivate.className = toActivate.className + " sort-active";
+        }
+
+        if (this.params.column.isSortAscending()) {
+            deactivate([this.$sortUp, this.$sortRemove]);
+            activate(this.$sortDown)
+        } else if (this.params.column.isSortDescending()) {
+            deactivate([this.$sortDown, this.$sortRemove]);
+            activate(this.$sortUp)
+        } else {
+            deactivate([this.$sortUp, this.$sortDown]);
+            activate(this.$sortRemove)
+        }
     }
 
     getGui() {
@@ -54,56 +73,20 @@ class CellHeader {
     }
 
     onSortRequested(order, event) {
-        Log(TAG, `onSortRequested: col: ${this.params.column.colId} order: ${order}`);
         PubSub.publish(Constants.SORT_REQUESTED, {
             column: this.params.column.colId,
             order: order
         });
 
-        //un-highlight other icons and highlight this
-        let active = document.querySelectorAll('.sort-active');
-
-        active.forEach((e) => {
-            e.classList.remove('sort-active');
-        });
-
-        switch (order) {
-        case 'asc':
-            this.$sortDown.classList.add('sort-active');
-            break;
-
-        case 'desc':
-            this.$sortUp.classList.add('sort-active');
-            break;
-
-        case '':
-            return;
-        }
-
-        this.$sortRemove.classList.add('sort-active');
+        this.params.setSort(order, event.shiftKey);
     }
 
     destroy() {
-        this.$sortDown.removeEventListener(
-            'click',
-            this.onSortRequestedListener
-        );
-
-        this.eSortUpButton.removeEventListener(
-            'click',
-            this.onSortRequestedListener
-        );
-
-        this.$sortRemove.removeEventListener(
-            'click',
-            this.onSortRequestedListener
-        );
-
-        this.params.column.removeEventListener(
-            'sortChanged',
-            this.onSortChangedListener
-        );
-    }
+        this.$sortDown.removeEventListener('click', this.onSortRequestedListener);
+        this.$sortUp.removeEventListener('click', this.onSortRequestedListener);
+        this.$sortRemove.removeEventListener('click', this.onSortRequestedListener);
+        this.params.column.removeEventListener('sortChanged', this.onSortChangedListener);
+    } 
 }
 
 export { CellHeader }
