24dcfe009e266137ca188434869150501794b144
Pankaj Kargirwar <kargirwar@protonmail.com>
2022-04-23 15:04:44 +0530

    Minified files

--- public/build-0.6/dist/js/help.js
+++ public/build-0.6/dist/js/help.js
@@ -1,15 +1,1 @@
-(function () {
-    'use strict';
-
-    class Help {
-        constructor() {
-            document.addEventListener('DOMContentLoaded', async () => {
-                let $email = document.querySelector('.email');
-                $email.classList.remove('is-hidden');
-            });
-        }
-    }
-
-    new Help();
-
-})();
+!function(){"use strict";new class{constructor(){document.addEventListener("DOMContentLoaded",(async()=>{document.querySelector(".email").classList.remove("is-hidden")}))}}}();
